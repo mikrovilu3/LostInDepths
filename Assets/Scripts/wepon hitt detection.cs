4c5c6e496103fabdf039acf4dd12a95f62c57426
mikrovilu3 <martins.mikelsons7@gmail.com>
2025-02-01 14:28:24 +0200

    debug

--- Assets/Scripts/wepon hitt detection.cs
+++ Assets/Scripts/wepon hitt detection.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class CollisionExample : MonoBehaviour
 {      Collision Collision;
     private void OnDrawGizmosSelected()
-    {   if (Collision != null)
-        {   
+    {
+        Debug.Log("drawed " + Collision);
+       if (Collision != null)
+        {
+            Debug.Log("drew " + Collision.contacts);
             int i = 0;
             Gizmos.color = Color.red;
             foreach (var item in Collision.contacts) { 
@@ -24,7 +27,7 @@ public class CollisionExample : MonoBehaviour
         // Access specific components on the other collider
         Collider otherCollider = collision.collider;
         // Log the name of the other GameObject
-        Debug.Log("Collided with: " + otherObject.name + "with"+otherCollider.name+ " colider"+collision);
+        Debug.Log("Collided with: " + otherObject.name + " with "+otherCollider.name+ " colider and collision "+collision);
     }
 
     // Called while this GameObject is colliding with another
