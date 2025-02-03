283135bf802fff5b9640ec68735d550343112e5f
mikrovilu3 <martins.mikelsons7@gmail.com>
2025-02-03 20:03:05 +0200

    otimized debug

--- Assets/Scripts/wepon hitt detection.cs
+++ Assets/Scripts/wepon hitt detection.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class CollisionExample : MonoBehaviour
 {      Collision Collision;
-    float force;
-    private void OnDrawGizmosSelected()
+    float force; int i = 0;
+    private void OnDrawGizmos()
     {
-        Debug.Log("drawed " + Collision + " with speed "+ force);
+        
        if (Collision != null & force > 0)
         {
-            Debug.Log("drew " + Collision.contacts);
-            int i = 0;
+            
+           
             Gizmos.color = Color.red;
             foreach (var item in Collision.contacts) { 
-                    Gizmos.DrawSphere(Collision.contacts[i].point, force);
-                    i++;
+                    Gizmos.DrawSphere(Collision.contacts[i].point, force*0.1f);
+                    
             }
-            if (force > 0) { force= force-0.1f; }
+            if (force > 0) { force= force-0.05f; }
         }
     }
     // Called when this GameObject starts colliding with another non-trigger collider
@@ -36,14 +36,14 @@ public class CollisionExample : MonoBehaviour
     // Called while this GameObject is colliding with another
     private void OnCollisionStay(Collision collision)
     {    
-        Debug.Log("Still colliding with: " + collision.gameObject.name);
-        
+        i++;
     }
 
     // Called when this GameObject stops colliding with anothers
     private void OnCollisionExit(Collision collision)
     {
         Debug.Log("Stopped colliding with: " + collision.gameObject.name);
+        Collision = null;
     }
 
 }
